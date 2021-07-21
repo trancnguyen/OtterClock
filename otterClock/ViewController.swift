012268f3afb9d6e43f13876ba20caba030929ba6
lalibotts <67395675+lalibotts@users.noreply.github.com>
2021-07-21 11:23:38 -0700

    New Update

--- otterClock/ViewController.swift
+++ otterClock/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtWorkTime: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func touchedSubmit(_ sender: UIButton) {
+        var toatlWorkTime = txtWorkTime.text
+        
+        
+        
+        
+    }
+    
 }
 

08ecbf0c19edb9a8eaef65b1120e3dae492be840
lalibotts <67395675+lalibotts@users.noreply.github.com>
2021-07-22 13:27:20 -0700

    update

--- otterClock/SecondViewController.swift
+++ otterClock/SecondViewController.swift
@@ -91,12 +91,10 @@ class SecondViewController: UIViewController {
     let gifURL : String = "https://drwngbrd.tumblr.com/page/3"
         let imageURL = UIImage.gifImageWithURL(gifURL)
         let imageView3 = UIImageView(image: imageURL)
-        imageView3.frame = CGRect(x: 20.0, y: 390.0, width: self.view.frame.size.width - 40, height: 150.0)
+    imageView3.frame = CGRect(x: 20.0, y: 390.0, width: self.view.frame.size.width - 40, height: 150.0)
         view.addSubview(imageView3)
-    
-    
-    
-    
+
+
     
     
     
